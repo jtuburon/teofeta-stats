0935a373db34acab956d8a936d1f2b14cbdf9b69
teo <jtuburon@gmail.com>
2020-11-20 21:40:48 -0500

    Prettier and ESLint settings files added

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es6": true,
+        "mocha": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1,
+                "VariableDeclarator": "first",
+                "flatTernaryExpressions": true
+            }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-var": [
+            "error"
+        ]
+    }
+ };
+ 
